776c408dc3c4ee5ae1deb505e59df2cf1f636aab
Sachi Nandan Pradhan <102918669+sachiweb@users.noreply.github.com>
2023-05-20 00:38:47 +0530

    Update settings.py

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
 import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,4 +137,3 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
